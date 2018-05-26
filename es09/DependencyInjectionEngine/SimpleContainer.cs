c9f6225f1766582e24a621170c9e19b833d5cf89
przemek <przemko924@gmail.com>
2018-05-26 18:57:32 +0200

    es09: DI engine: handling varous constructors

--- es09/DependencyInjectionEngine/SimpleContainer.cs
+++ es09/DependencyInjectionEngine/SimpleContainer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using System.Collections.Generic;
 
 namespace simplecontainer
@@ -9,13 +10,16 @@ namespace simplecontainer
 
         private Dictionary<Type, Type> _types = new Dictionary<Type, Type>();
 
+        // Avoid cycles in the resolution
+        private List<ConstructorInfo> _resolved = new List<ConstructorInfo>();
+
         public void RegisterType<T>(bool Singleton = false) where T : class {
             if (Singleton && _singletons.ContainsKey(typeof(T)) == false) {
                 _singletons.Add(typeof(T), newInstance<T>());
             }
         }
 
-        public void RegisterType<From, To>( bool Singleton = false ) 
+        public void RegisterType<From, To>(bool Singleton = false) 
                 where To : class 
                 where From : class 
             {
@@ -26,9 +30,55 @@ namespace simplecontainer
         }
 
         public T Resolve<T>() {
+            
             if(_singletons.ContainsKey(typeof(T))) {
                 return (T)_singletons[typeof(T)];
             }
+
+            ConstructorInfo[] ctorsInfo = typeof(T).GetConstructors();
+
+            
+            // Type T has no constructor OR
+            // has only one constuctor that has no parameters
+            if(ctorsInfo.Length == 0  || (ctorsInfo.Length == 1 
+                                   && ctorsInfo[0].GetParameters().Length == 0))
+            {
+                return ResolveType<T>();
+            }
+            
+            // Find a contructor which has the most parameters
+            // If more than one exists, then an arbitrary one is chosen
+            ConstructorInfo ctor = ctorsInfo[0];
+
+            foreach(var c in ctorsInfo) {
+                if(c.GetParameters().Length > ctor.GetParameters().Length) {
+                    ctor = c;
+                }
+            }
+            List<object> ctorParams = new List<object>();
+
+            foreach(var param in ctor.GetParameters()) {
+                Type P = param.ParameterType;
+                try {
+                    MethodInfo method = typeof(SimpleContainer).GetMethod("Resolve");
+                    MethodInfo generic = method.MakeGenericMethod(P);
+                    generic.Invoke(this, null);
+                    ctorParams.Add(generic.Invoke(this, null));
+                } catch(Exception) {
+                    throw new Exception(
+                        "SimpleContainer: failed to resolve type recursively: " 
+                        + P.ToString());
+                }
+            }
+
+            return (T) Activator.CreateInstance(typeof(T), ctorParams.ToArray());
+        }
+
+        private T ResolveType<T>() {
+            /*
+             * [Recursion base] Resolves a type with only one paramterless ctor
+             */
+
             if(_types.ContainsKey(typeof(T))) {
                 return (T) Activator.CreateInstance(_types[typeof(T)]);
             }
@@ -36,7 +86,7 @@ namespace simplecontainer
             try {
                 t = newInstance<T>();
             } catch(Exception e) {
-                throw new Exception("No such type registered");
+                throw new Exception("SimpleContainer: no such type registered: " + typeof(T).ToString());
             } 
             return t;
         }
---
+++ es09/DependencyInjectionEngineTest/RecursiveSolutionTest.cs
@@ -0,0 +1,55 @@
+using System;
+using NUnit.Framework;
+using simplecontainer;
+
+namespace simplecontainertest
+{
+    public class RecursiveSolutionTest {
+
+        public class A{
+            public B b;
+            public A(B b) {
+                this.b = b;
+            }
+        }
+
+        public class B { }
+
+        public class X {
+            public X(string s) { }
+        }
+
+        [Test]
+        public void RecursiveSolution() {
+            SimpleContainer c = new SimpleContainer();
+            A a = c.Resolve<A>();
+            Assert.True(a.b != null);
+        }
+
+        [Test]
+        public void RecursiveSolutionRegisterInstance() {
+            SimpleContainer c = new SimpleContainer();
+            X x;
+            Assert.Throws<Exception>(() => {x = c.Resolve<X>();} );
+            c.RegisterInstance("register an instance"); 
+            x = c.Resolve<X>();
+            Assert.True(x is X);
+        }
+
+        public class Foo {
+            public Bar bar;
+            public Foo(Bar bar) {}
+        }
+
+        public class Bar {
+            public Bar(Foo foo) {}
+        }
+
+        [Test]
+        public void RecursiveSolutionDetectCycleTest() {
+            SimpleContainer c = new SimpleContainer();
+            Foo foo = c.Resolve<Foo>();
+        }
+
+    }
+}
